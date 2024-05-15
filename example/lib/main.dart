1f15602896385b48605811371fb0824257486991
Jigar-f <jigar@getlantern.org>
2024-05-15 11:39:29 +0530

    make sure callback is top-level.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
+
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'package:pushy_flutter/pushy_flutter.dart';
-
 import 'package:pusharound/pusharound.dart';
+import 'package:pushy_flutter/pushy_flutter.dart';
 
 // This is only needed for Web Push.
 // See https://pushy.me/docs/additional-platforms/flutter
@@ -72,6 +71,7 @@ class _AppState extends ChangeNotifier {
     () async {
       var token = await Pushy.register();
       deviceToken = token;
+      print("Device token: $token");
       notifyListeners();
     }();
   }
@@ -116,28 +116,32 @@ class _HomePageState extends State<_HomePage> {
     });
 
     return Scaffold(
-      bottomNavigationBar:
-          SelectableText("device token: ${appState.deviceToken}"),
-      body: Row(
-        children: [
-          Expanded(
-            child: _NotificationsColumn(
-              'Pusharound Notifications',
-              appState.pusharoundNotifications,
+      persistentFooterButtons: [
+        SelectableText("device token: ${appState.deviceToken}")
+      ],
+      persistentFooterAlignment: AlignmentDirectional.center,
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: _NotificationsColumn(
+                'Pusharound Notifications',
+                appState.pusharoundNotifications,
+              ),
             ),
-          ),
-          VerticalDivider(
-            color: theme.colorScheme.onBackground,
-            thickness: 2.0,
-            width: 0,
-          ),
-          Expanded(
-            child: _NotificationsColumn(
-              'Non-Pusharound Notifications',
-              appState.nonPusharoundNotifications,
+            VerticalDivider(
+              color: theme.colorScheme.onBackground,
+              thickness: 2.0,
+              width: 0,
             ),
-          ),
-        ],
+            Expanded(
+              child: _NotificationsColumn(
+                'Non-Pusharound Notifications',
+                appState.nonPusharoundNotifications,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -152,7 +156,7 @@ class _NotificationsColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
+    final style = theme.textTheme.titleMedium!.copyWith(
       color: theme.colorScheme.primary,
     );
 
--- src/clients/flutter/pusharound/lib/src/pusharound.dart
+++ src/clients/flutter/pusharound/lib/src/pusharound.dart
@@ -1,7 +1,12 @@
 library pusharound;
 
-import 'push_provider.dart';
 import 'push_notification.dart';
+import 'push_provider.dart';
+
+// typedef for the onNotification ,stream and exception
+typedef void OnNotification(PushNotification);
+typedef void OnStream(String);
+typedef void OnNotificationError(Exception);
 
 // All pusharound messages contain this key. For streams (data split across
 // multiple messages), this key will be mapped to an identifier indicating the
@@ -24,110 +29,133 @@ const _streamCompleteKey = "pusharound-stream-ok";
 // many messages. One-off messages use custom keys to specify user data.
 const _streamDataKey = "pusharound-stream-data";
 
+// Define global variables to hold the callback functions
+OnNotification? _globalOnNotification;
+OnStream? _globalOnStream;
+OnNotificationError? _globalOnError;
+
 /// The governing class for receiving pusharound messages.
 class Pusharound {
   final List<PushProvider> _providers;
 
   // Stream ID -> { datum index -> datum }
-  final Map<String, Map<int, String>> _incompleteStreams = {};
+ static final Map<String, Map<int, String>> _incompleteStreams = {};
 
   // Stream ID -> index of last message if received.
-  final Map<String, int> _lastIndex = {};
+  static final Map<String, int> _lastIndex = {};
 
   /// Initializes a Pusharound instance with the given push notification
   /// providers.
   Pusharound(this._providers);
 
   /// Registers listeners for notifications, streams, and exceptions.
-  void setListeners(Function(PushNotification) onNotification,
-      Function(String) onStream, Function(Exception) onException) {
-    // Define a function for handling raw notification data.
-    notificationHandler(Map<String, dynamic> data) {
-      var streamID = data[_streamIDKey];
-      var streamCounterRaw = data[_streamCounterKey];
-      var streamComplete = data[_streamCompleteKey];
-      var streamData = data[_streamDataKey];
+  void setListeners(OnNotification onNotification,
+      OnNotification onStream, OnNotificationError onException) {
+    //Save callback functions to global variables
+    _globalOnNotification = onNotification;
+    _globalOnStream = onStream;
+    _globalOnError = onException;
 
-      // Clean up pusharound metadata. These keys won't all be included on every
-      // message, but it doesn't hurt to try removing them all just in case.
-      data.remove(_streamIDKey);
-      data.remove(_streamCounterKey);
-      data.remove(_streamCompleteKey);
-      data.remove(_streamDataKey);
 
-      if (streamID == null) {
-        onNotification(PushNotification(false, data));
-        return;
-      }
-
-      if (streamID is! String) {
-        var rt = streamID.runtimeType;
-        onException(Exception("received $rt instead of String for streamID"));
-        return;
-      }
-
-      if (streamID == _nullStreamID) {
-        // One-off message.
-        onNotification(PushNotification(true, data));
-        return;
-      }
-
-      // At this point, we are handling a stream.
-
-      if (streamCounterRaw == null) {
-        onException(Exception("received stream without counter"));
-        return;
-      }
-
-      // This may be an empty completion message. In this case, there is no data
-      // included, but we must still update the data in _incompleteStreams.
-      streamData ??= "";
-
-      if (streamData is! String) {
-        var rt = streamData.runtimeType;
-        onException(
-            Exception("received $rt instead of String for stream data"));
-        return;
-      }
-
-      int streamCounter;
-      try {
-        streamCounter = int.parse(streamCounterRaw);
-      } on FormatException catch (e) {
-        onException(Exception("received malformed stream counter: $e"));
-        return;
-      }
-
-      if (streamComplete != null) {
-        _lastIndex[streamID] = streamCounter;
-      }
-
-      var stream = _incompleteStreams.putIfAbsent(streamID, () => {});
-      stream[streamCounter] = streamData;
-
-      // If we have not received the entire stream, we must wait for future
-      // notifications.
-      var lastIndex = _lastIndex[streamID];
-      if (lastIndex == null) {
-        return;
-      }
-
-      var collated = '';
-      for (var i = 0; i <= lastIndex; i++) {
-        var data = stream[i];
-        if (data == null) {
-          // We're missing a message; wait for future notifications.
-          return;
-        }
-        collated += data;
-      }
-
-      onStream(collated);
-    }
-
-    // Now pass our notification handler to each of the configured providers.
+    /// Now pass our notification handler to each of the configured providers.
+    /// [OnNotification]
+    /// callback need to be annotated with the `@pragma('vm:entry-point')`
+    /// annotation to ensure they are not stripped out by the Dart compiler.
+    /// This is required by Pushy SDK
     for (var provider in _providers) {
-      provider.setListener(notificationHandler);
+      provider.setListener(
+        notificationHandler,
+      );
     }
+  }
+
+
+  @pragma('vm:entry-point')
+ static void notificationHandler(Map<String, dynamic> data) {
+    // Make sure we have all callback functions
+    if (_globalOnNotification == null || _globalOnStream == null ||
+        _globalOnError == null) {
+      throw Exception("Callback functions are not set");
+    }
+    var streamID = data[_streamIDKey];
+    var streamCounterRaw = data[_streamCounterKey];
+    var streamComplete = data[_streamCompleteKey];
+    var streamData = data[_streamDataKey];
+
+    // Clean up pusharound metadata. These keys won't all be included on every
+    // message, but it doesn't hurt to try removing them all just in case.
+    data.remove(_streamIDKey);
+    data.remove(_streamCounterKey);
+    data.remove(_streamCompleteKey);
+    data.remove(_streamDataKey);
+
+    if (streamID == null) {
+      _globalOnNotification!(PushNotification(false, data));
+      return;
+    }
+
+    if (streamID is! String) {
+      var rt = streamID.runtimeType;
+      _globalOnError!(Exception("received $rt instead of String for streamID"));
+      return;
+    }
+
+    if (streamID == _nullStreamID) {
+      // One-off message.
+      _globalOnNotification!(PushNotification(true, data));
+      return;
+    }
+
+    // At this point, we are handling a stream.
+
+    if (streamCounterRaw == null) {
+      _globalOnNotification!(Exception("received stream without counter"));
+      return;
+    }
+
+    // This may be an empty completion message. In this case, there is no data
+    // included, but we must still update the data in _incompleteStreams.
+    streamData ??= "";
+
+    if (streamData is! String) {
+      var rt = streamData.runtimeType;
+      _globalOnError!(
+          Exception("received $rt instead of String for stream data"));
+      return;
+    }
+
+    int streamCounter;
+    try {
+      streamCounter = int.parse(streamCounterRaw);
+    } on FormatException catch (e) {
+      _globalOnError!(Exception("received malformed stream counter: $e"));
+      return;
+    }
+
+    if (streamComplete != null) {
+      _lastIndex[streamID] = streamCounter;
+    }
+
+    var stream = _incompleteStreams.putIfAbsent(streamID, () => {});
+    stream[streamCounter] = streamData;
+
+    // If we have not received the entire stream, we must wait for future
+    // notifications.
+    var lastIndex = _lastIndex[streamID];
+    if (lastIndex == null) {
+      return;
+    }
+
+    var collated = '';
+    for (var i = 0; i <= lastIndex; i++) {
+      var data = stream[i];
+      if (data == null) {
+        // We're missing a message; wait for future notifications.
+        return;
+      }
+      collated += data;
+    }
+
+    _globalOnStream!(collated);
   }
 }
